0e962663be88515ad5f8c5728921a41a3c6c933f
Strong Uzumaki <suzumaki@at-k5.msk.21-school.ru>
2020-12-24 21:28:21 +0300

    1

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,123 @@
 /*   By: suzumaki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 13:34:05 by suzumaki          #+#    #+#             */
-/*   Updated: 2020/12/19 18:03:57 by suzumaki         ###   ########.fr       */
+/*   Updated: 2020/12/24 20:51:44 by suzumaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
+#include <stdlib.h>
+
+void	ft_memdel(void **ap)
+{
+	if (ap != NULL)
+	{
+		free(*ap);
+		*ap = NULL;
+	}
+}
+
+
+void	ft_strdel(char **as)
+{
+	ft_memdel((void**)as);
+}
+
+
+
+void	ft_strclr(char *s)
+{
+	if (s)
+	{
+		while (*s)
+		{
+			*s = '\0';
+			s++;
+		}
+	}
+}
+
+char	*ft_strcat(char *restrict str1, const char *restrict str2)
+{
+	char *tmp;
+
+	tmp = str1;
+	while (*str1)
+		str1++;
+	while (*str2)
+	{
+		*str1 = *str2;
+		str1++;
+		str2++;
+	}
+	*str1 = '\0';
+	return (tmp);
+}
+
+
+char	*ft_strnew(size_t size)
+{
+	char *str;
+
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (str == NULL)
+		return (NULL);
+	while (size > 0)
+		str[size--] = '\0';
+	str[0] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char*)s);
+		s++;
+	}
+	if (*s == c)
+		return ((char*)s);
+	else
+		return (0);
+}
+
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*sub_str;
+	size_t	i;
+
+	i = 0;
+	sub_str = (char *)malloc(len + 1);
+	if (sub_str == NULL || s == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		sub_str[i] = s[start];
+		i++;
+		start++;
+	}
+	sub_str[i] = '\0';
+	return (sub_str);
+}
+
+char	*ft_strcpy(char *dst, const char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
 
 size_t	ft_strlen(const char *s)
 {
@@ -38,6 +149,39 @@ void	*ft_memset(void *s, int c, size_t n)
 	return (s);
 }
 
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char *cpy_dest;
+	unsigned char *cpy_src;
+
+	cpy_dest = (unsigned char *)dest;
+	cpy_src = (unsigned char *)src;
+	if (dest == NULL && src == NULL)
+		return (dest);
+	while (n--)
+		*cpy_dest++ = *cpy_src++;
+	return (dest);
+}
+
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t srclen;
+	size_t dstlen;
+
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	if (dstlen >= size)
+		return (size + srclen);
+	if (srclen < size - dstlen)
+		ft_memcpy(dst + dstlen, src, srclen + 1);
+	else
+	{
+		ft_memcpy(dst + dstlen, src, size - 1);
+		dst[size - 1] = '\0';
+	}
+	return (dstlen + srclen);
+}
 
 char	*ft_strdup(const char *s)
 {
@@ -102,29 +246,84 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (res);
 }
 
-
-int get_next_line(int fd, char **line)
+t_arr *newlist(const int fd)
 {
-	char *tmp;
-	static char *rmn;
+	t_arr *new;
 
-	tmp = malloc((BUFFER_SIZE+1)*sizeof(char));
-	ft_bzero(tmp,BUFFER_SIZE+1);
-	read(fd,tmp,BUFFER_SIZE);
-	if(ft_strrchr(tmp,'\n'))
-		*line = ft_strdup(tmp);		
-	else
-	{	
-		ft_strjoin(rmn,tmp);
+	if (!(new = (t_arr *)malloc(sizeof(t_arr))))
+		return (NULL);
+	new->fd = fd;
+	new->rest = ft_strnew(BUFFER_SIZE);
+	new->next = NULL;
+	return (new);
+}
+
+char *checkrest(char **p_n, char *rest)
+{
+	char *str;
+
+	if ((*p_n = ft_strchr(rest, '\n')) != NULL)
+	{
+		str = (ft_strsub(rest, 0, *p_n - rest));
+		ft_strcpy(rest, ++(*p_n));
 	}
-	free(tmp);
-	return(0);
+	else
+	{
+		str = ft_strnew(ft_strlen(rest) + 1);
+		ft_strlcat(str, rest,1000);
+		ft_strclr(rest);
+	}
+	return (str);
+
 }
 
 
-int main()
+int get_line(const int fd, char **line, char *rest)
 {
-	char *line;
-	get_next_line(1, &line);
-	printf("%s",line);
+	char	buf[BUFFER_SIZE+1];
+	char	*p_n;
+	char	*tmp;
+	int rd;
+
+	p_n = NULL;
+	rd = 1;
+	*line = checkrest(&p_n, rest);
+	while (p_n == 0 && ((rd = read(fd,buf, BUFFER_SIZE))) != 0)
+	{
+		buf[rd] = '\0';
+		if ((p_n = ft_strchr(buf, '\n')) != NULL)
+		{
+			ft_strcpy(rest, ++p_n);
+			ft_strclr(--p_n);
+		}
+		tmp = *line;
+		if (!(*line = ft_strjoin(tmp, buf)) || rd < 0)
+			return (-1);
+		//ft_strdel(&tmp);
+		free(tmp);
+	}
+	return ((ft_strlen(*line) || ft_strlen(rest) || rd) ? 1 : 0);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static t_arr	*list;
+	t_arr			*tmp;
+	int				ret;
+
+	if (BUFFER_SIZE < 1)
+		return (-1);
+	if (fd < 0 || line == 0)
+		return (-1);
+	if (!list)
+		list = newlist(fd);
+	tmp = list;
+	while (tmp->fd != fd)
+	{
+		if (tmp->next == NULL)
+			tmp->next = newlist(fd);
+		tmp = tmp->next;
+	}
+	ret = get_line(fd, line, tmp->rest);
+	return (ret);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,36 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: suzumaki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/19 12:22:43 by suzumaki          #+#    #+#             */
-/*   Updated: 2020/12/24 19:52:07 by suzumaki         ###   ########.fr       */
+/*   Created: 2020/12/24 19:42:57 by suzumaki          #+#    #+#             */
+/*   Updated: 2020/12/24 19:43:11 by suzumaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-#include <unistd.h>
-#include <stdlib.h>
-
-typedef struct	s_arr
-{
-	int			fd;
-	char		*rest;
-	struct s_arr	*next;
-}				t_arr;
-
-t_arr	*ft_newlist(const int fd);
-char *checkrest(char **p_n, char *rest);
-int get_line(const int fd, char **line, char *rest);
-int get_next_line(int fd, char **line);
-
-#endif
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 16
-
-#endif
+//сань хуй соси
